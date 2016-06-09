ab8c498af2546fa6a9238f94d0308947992408b6
Galvan Dominguez <Dgalvan@teletech.com>
2016-06-09 13:13:34 -0500

    add icons

--- src/app/+project/project.component.ts
+++ src/app/+project/project.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
 
 @Component({
   moduleId: module.id,
@@ -8,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectComponent implements OnInit {
 
-  constructor() {}
+  constructor(
+    private router: Router) { }
+
 
   ngOnInit() {
   }
 
+  onSelect() {
+    //this.router.navigate( ['HeroDetail', { id: hero.id }] );
+    //this.router.navigate( ['/'] );
+    this.router.navigateByUrl('/');
+  }
 }
--- src/app/sysba-pm.component.ts
+++ src/app/sysba-pm.component.ts
@@ -1,22 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { ProjectComponent } from './+project';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
-import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
-import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
+import { Routes, Router, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
+import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
+
 
 @Component({
   moduleId: module.id,
   selector: 'sysba-pm-app',
   templateUrl: 'sysba-pm.component.html',
   styleUrls: ['sysba-pm.component.css'],
-  directives: [ROUTER_DIRECTIVES,MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES, MD_TOOLBAR_DIRECTIVES],
-  providers: [ROUTER_PROVIDERS]
+  directives: [ROUTER_DIRECTIVES, MD_BUTTON_DIRECTIVES, MD_TOOLBAR_DIRECTIVES, MdIcon],
+  providers: [ROUTER_PROVIDERS],
+  viewProviders: [MdIconRegistry]
 })
+
 @Routes([
   {path: '/project', component: ProjectComponent}
 ])
 export class SYSBAPMAppComponent {
-  title = 'Project Management';
+  title = 'Sysba PM';
+
+  constructor(mdIconRegistry: MdIconRegistry,
+            private router: Router) {
+    mdIconRegistry
+       .addSvgIcon('thumb-up', '/node_modules/mdi/fonts/materialdesignicons-webfont.svg');       
+  }
+
+  Projecto() {
+    this.router.navigateByUrl('/project');
+  }
 }
